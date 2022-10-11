38be37ec8dc95dc534055d736f1b25f87bab7f4e
calnelson <calnelson77@gmail.com>
2022-10-11 00:01:00 -0500

    trying to get my shizz up to Github

---
+++ index.js
@@ -0,0 +1,7 @@
+import pigLatin from './main.js';
+
+let input = '';
+
+let textInput = document.getElementById('user-input')
+
+console.log(textInput.value)
--- main.js
+++ main.js
@@ -20,12 +20,15 @@ const pigLatin = (word) => {
   let finalArray = []
   let bucketArray = []
   for (let index = 0; index < 2; index++) {
+    // console.log(wordArray[index])
     if(vowels.includes(wordArray[index])) {
       if(index === 0){
         let theWord = word + 'yay'
         finalArray.push(theWord)
       } else {
-        let theWord = word.slice(index, word.length) + word.slice(0, index) + 'ay';
+        let theWord = word.slice(index, word.length) 
+                    + word.slice(0, index) 
+                    + 'ay';
         finalArray.push(theWord);
       }
     } else if (!vowels.includes(wordArray[index])) {
@@ -43,7 +46,7 @@ const pigLatin = (word) => {
     }
   }
   const answer = finalArray.join();
-  // console.log(answer)
+  // console.log(typeof(answer))
   return answer
 }
 
@@ -99,3 +102,160 @@ if (typeof describe === 'function') {
 // 1. if word begins with a vowel send to one function: adds "yay"
 // 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
 // 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+
+
+
+
+
+
+
+// // const isUnique = (word) => {
+// //   // console.log(`Original word: ${word}`)
+// //   word = word.split('')
+// //   // console.log(`Word array: ${word}`)
+// //   let setWord = new Set(word)
+// //   if (word.length === setWord.size) {
+// //       console.log('true')
+// //       return true
+// //   } else {
+// //       // console.log('false')
+// //       console.log('fuck')
+// //       return false
+// //   }
+// // }
+
+// // isUnique('egg') // --> false
+// // isUnique('Artemis') // --> true
+
+// 'use strict';
+
+// // brings in the assert module for unit testing
+// const assert = require('assert');
+// // brings in the readline module to access the command line
+// const readline = require('readline');
+// // use the readline module to print out to the command line
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// const pigLatin = (word) => {
+//   // Make sure word is trimmed and put to lowercase first
+//   word = word.trim()
+//   word = word.toLowerCase()
+//   // Set your vowels array to compare to word
+//   let vowels = ['a', 'e', 'i', 'o', 'u']
+//   let wordArray = word.split('')
+//   let finalArray = []
+//   let bucketArray = []
+//   for (let index = 0; index < 2; index++) {
+//     // console.log(wordArray[index])
+//     if(vowels.includes(wordArray[index])) {
+//       if(index === 0){
+//         let theWord = word + 'yay'
+//         finalArray.push(theWord)
+//       } else {
+//         let theWord = word.slice(index, word.length) 
+//                     + word.slice(0, index) 
+//                     + 'ay';
+//         finalArray.push(theWord);
+//       }
+//     } else if (!vowels.includes(wordArray[index])) {
+//       let bucket = wordArray[index]
+//       bucketArray.push(bucket)
+//       if (bucketArray.length === 2) {
+//         let otherArray = bucketArray.join("")
+//         console.log(otherArray)
+//         let theWord = word.slice(2, word.length)
+//                     + word.slice(2, index)
+//                     + otherArray
+//                     + 'ay'
+//         finalArray.push(theWord)
+//       }
+//     }
+//   }
+//   const answer = finalArray.join();
+//   // console.log(typeof(answer))
+//   return answer
+// }
+
+// // the first function called in the program to get an input from the user
+// // to run the function use the command: node main.js
+// // to close it ctrl + C
+// const getPrompt = () => {
+//   rl.question('word ', (answer) => {
+//     console.log( pigLatin(answer) );
+//     getPrompt();
+//   });
+// }
+
+// // Unit Tests
+// // to use them run the command: npm test main.js
+// // to close them ctrl + C
+// if (typeof describe === 'function') {
+
+//   describe('#pigLatin()', () => {
+//     it('should translate a simple word', () => {
+//       assert.equal(pigLatin('car'), 'arcay');
+//       assert.equal(pigLatin('dog'), 'ogday');
+//     });
+//     it('should translate a complex word', () => {
+//       assert.equal(pigLatin('create'), 'eatecray');
+//       assert.equal(pigLatin('valley'), 'alleyvay');
+//     });
+//     it('should attach "yay" if word begins with vowel', () => {
+//       assert.equal(pigLatin('egg'), 'eggyay');
+//       assert.equal(pigLatin('emission'), 'emissionyay');
+//     });
+//     it('should lowercase and trim word before translation', () => {
+//       assert.equal(pigLatin('HeLlO '), 'ellohay');
+//       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+//     });
+//   });
+// } else {
+
+//   getPrompt();
+
+// }
+
+
+
+
+
+
+// // **********
+// //   HINTS
+// // **********
+
+// // break your code into pieces and focus on one piece at a time...
+// // 1. if word begins with a vowel send to one function: adds "yay"
+// // 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
+// // 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+
+
+
+
+
+//   const findLongestWord = (myString) => {
+
+//   let stringArray = myString.split(' ')
+//   let longestWord = 0
+//   let answer
+//   for (let i = 0; i < stringArray.length; i++){
+
+//   if(stringArray[i].length > longestWord){
+//       longestWord = stringArray[i].length
+//        answer= stringArray[i]
+//   }
+// }
+// return [answer, longestWord]
+// }
+
+// console.log(findLongestWord("This is for your own personal journey. May you have excellent navigation as a developer using your own compass."))
+
+  // let wordArray =[];
+
+  // for ()
+
+  // findLongestWord(myString) => 'navigation', 10
+
